5b1c13f56cb74ddc6d46befeeba353dbb779096d
JIrleidySantos <jirleidysantosm@gmail.com>
2024-11-20 11:10:04 -0500

    chili apache

---
+++ src/main/java/ec/edu/espoch/practicachili/Practicachili.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.practicachili;
+
+/**
+ *
+ * @author mundo
+ */
+public class Practicachili {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Practica en un aula");
+    }
+}
